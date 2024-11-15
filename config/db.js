98a354ee5c0befa76337f336d9eb99b52cb7db1c
Yash Patel <ypate116@my.centennialcollege.ca>
2024-11-15 09:35:08 -0500

    minor fix in db.js to remove useNewUrlParser and useUnifiedTopology which are no longer required

--- config/db.js
+++ config/db.js
@@ -5,10 +5,9 @@ const connectDB = async () => {
     if (!process.env.MONGO_URI) {
       throw new Error('MONGO_URI is not defined in environment variables');
     }
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+
+    // Connect to the database
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error.message);

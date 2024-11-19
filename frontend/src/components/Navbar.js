5cf1b177a825065a893f5bfda583f3e72a2c6e65
Yash Patel <ypate116@my.centennialcollege.ca>
2024-11-18 20:37:48 -0500

    minor UI changes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,25 +2,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'; 
+import logo from './logo.jpg';
 
 const Navbar = ({ isAuthenticated, onLogout }) => {
   return (
     <nav>
+      <img src={logo} alt="Library Manager Logo" className="navbar-logo" />
       <h1>Library Manager</h1>
       <div className="nav-links"> 
         <Link to="/">Home</Link>
+        {isAuthenticated && <Link to="/addbook">Add Book</Link>}
         {isAuthenticated ? (
           <>
             <Link to="/myprofile">My Profile</Link>
             <button onClick={onLogout}>Logout</button>
           </>
+          
         ) : (
           <>
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
           </>
         )}
-        {isAuthenticated && <Link to="/addbook">Add Book</Link>}
+        
 
       </div>
     </nav>
